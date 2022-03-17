3e11e8e29af651a950c2475d1855f78376fd8c6d
Gilder <gildermontepeque52@gmail.com>
2022-03-16 21:09:24 -0700

    Update identifyingSources.py

--- identifyingSources.py
+++ identifyingSources.py
@@ -2,6 +2,6 @@ import requests
 resp = requests.post('https://textbelt.com/text', {
   'phone': '3235485028',
   'message': 'Test',
-  'key': 'textbelt',
+  'key': '6758d5f3183daa56d79695bf52c0ade7ecade142VGy0XajDboQXRhqKnrXPS1rC1',
 })
 print(resp.json())
